085538bfe5a57e3a426cf357d43f230908caf010
NTH19 <3310288189@qq.com>
2022-05-07 17:18:31 +0800

    finish remarks

---
+++ app/src/main/java/com/example/cashbook/RecordActivity.kt
@@ -0,0 +1,11 @@
+package com.example.cashbook
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class RecordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_record)
+    }
+}
